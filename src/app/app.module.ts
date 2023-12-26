f2567e6b802d03b74d2f1d9ee6332da58070b62a
ehuamanttupa <ehuamanttupa@stfonavi.gob.pe>
2023-12-26 17:37:29 -0500

    Módulo Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,18 @@ import { AppHeaderComponent } from './layouts/full/header/header.component';
 import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SignupComponent } from './signup/signup.component';
+import { NgxUiLoaderConfig, NgxUiLoaderModule, SPINNER } from 'ngx-ui-loader';
+
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+  text:"Loading....",
+  textColor:"#FFFFFF",
+  textPosition:"center-center",
+  bgsColor:"#7b1fa2",
+  fgsColor:"#7b1fa2",
+  fgsType:SPINNER.squareJellyBox,
+  fgsSize:100,
+  hasProgressBar:false
+}
 
 @NgModule({
   declarations: [
@@ -35,7 +47,8 @@ import { SignupComponent } from './signup/signup.component';
     MaterialModule,
     FlexLayoutModule,
     SharedModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { SnackbarService } from '../services/snackbar.service';
+import { MatDialogRef } from '@angular/material/dialog';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { GlobalConstants } from '../shared/global-constants';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +14,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  password = true;
+  confirmPassword = true;
+  signupForm: any = FormGroup
+  responseMessage:any;
+
+  constructor(
+    private formBuilder:FormBuilder,
+    private router:Router,
+    private useService:UserService,
+    private snackbarService:SnackbarService,
+    public dialogRef:MatDialogRef<SignupComponent>,
+    private ngxService:NgxUiLoaderService
+  ) { }
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.pattern(GlobalConstants.nameRegex)]],
+      email: [null,[Validators.required,Validators.pattern(GlobalConstants.emailRegex)]],
+      contactNumber:[null,[Validators.required,Validators.pattern(GlobalConstants.contactNumberRegex)]],
+      password: [null,[Validators.required]],
+      confirmPassword: [null,[Validators.required]]
+    })
+  }
+
+  validateSubmit(){
+
+    if(this.signupForm.controls['password'].value != this.signupForm.controls['confirmPassword'].value){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  handleSubmit(){
+    this.ngxService.start();
+    var formdData = this.signupForm.value;
+    var data = {
+        name: formdData.name,
+        email: formdData.email,
+        contactNumber: formdData.contactNumber,
+        password: formdData.password
+    }
+
+    this.useService.signup(data).subscribe((response:any)=> {
+      this.ngxService.stop();
+      this.dialogRef.close();
+      this.responseMessage = response?.message;
+      this.snackbarService.openSnackBar(this.responseMessage,"");
+      this.router.navigate(['/']);
+    },(error)=>{
+      this.ngxService.stop();
+      console.log("Error during signup", error);
+      if(error.error?.message){
+        this.responseMessage = error.error?.message;
+      }else{
+        this.responseMessage = GlobalConstants.genericError;
+
+      }
+      this.snackbarService.openSnackBar(this.responseMessage,GlobalConstants.error);
+    })
   }
 
 }
